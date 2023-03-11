a0fc6bf5ca3a93e15a5e742ce96ec00d5436eb12
Admin <danylotroyan24@gmail.com>
2023-03-11 15:15:59 +0200

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello')
+import random
+eat = random.randint(0,1)
+class Cat:
+    global eat
+    def __init__(self):
+        if eat == 1:
+            print('Myav!')
+            vubir =input('Eat or dont eat: \n')
+            if vubir == 'Eat':
+                print('Myr')
+            else:
+                print('Myav!')
+        else:
+            print('Myr')
+cat = Cat()
